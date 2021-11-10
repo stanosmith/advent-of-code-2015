2c11b16ee68c8d7c78532439d6da5403dc5b576b
Stan Smith <choicedesign@gmail.com>
2021-11-09 22:10:34 -0700

    Solve Day 1:Part 2

--- src/day01/index.js
+++ src/day01/index.js
@@ -5,12 +5,12 @@ const inputPath = './input.txt'
 try {
   const inputString = await getInput(inputPath)
   console.log(
-    inputString.split('').reduce((floor, instruction) => {
-      if (instruction === '(') {
-        return floor + 1
-      } else {
-        return floor - 1
+    inputString.split('').reduce((floor, instruction, index) => {
+      let newFloor = instruction === '(' ? floor + 1 : floor - 1
+      if (newFloor === -1) {
+        console.log({ newFloor, characterPosition: index + 1 })
       }
+      return newFloor
     }, 0),
   )
 } catch (e) {
